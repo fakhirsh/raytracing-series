7a6f6cf702faed37d993a8fa07dbaa844d93aea3
fakhirsh <fakhir.shaheen@gmail.com>
2024-07-21 11:21:17 +0500

    Weird pattern emerges

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
-#include <stdio.h>
+#include <iostream>
 
 int main(int, char**){
-    printf("Hello, from RaytracerWeekend!\n");
-    int a = 1;
-    int b = 2;
-    int c = a + b;
     
+    const int WIDTH  = 1024;
+    const int HEIGHT = 1024;
+
+    std::cout << "P3" << std::endl;
+    std::cout << WIDTH << " " << HEIGHT << std::endl;
+    std::cout << "255" << std::endl;
+
+    for(int h = 0; h < HEIGHT; h++){
+        for(int w = 0; w < WIDTH; w++){
+            std::cout << (h*w+w)%255 << " " << 0 << " " << 0 << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
